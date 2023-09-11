016172f1119a0bf2923e49fa7c6b9257a22cd5b0
lajkak <92091234+LajkaK2006@users.noreply.github.com>
2023-09-11 19:46:31 +0200

    Add files via upload

---
+++ JavaScript/data.js
@@ -0,0 +1,19 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price":  "",
+    "des": "",
+    "marka": "",
+    "image": "img/no img.png"
+}, {
+    "id": 1,
+    "title": "Kamera1",
+    "price": 37000,
+    "des": "Ok",
+    "marka": "Asus",
+    "image": "https://imgs.search.brave.com/FBSpl8DQ0WbGUFN3N_Q62dIwhTSFY-Jda2BQhHYg7Lk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzYwLzMzLzcz/LzM2MF9GXzYwMzM3/Mzk0XzE1SzR5RWZw/MWJwdkIzOHVjbFgx/RXQ2OTFpRVBaYklR/LmpwZw"
+
+
+
+
+}]
