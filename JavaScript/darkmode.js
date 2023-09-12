5476663d34ad184535afdcea86c44efd9d67fb23
lajkak <92091234+LajkaK2006@users.noreply.github.com>
2023-09-12 23:57:20 +0200

    Add files via upload

--- JavaScript/darkmode.js
+++ JavaScript/darkmode.js
@@ -9,4 +9,25 @@ darkmode.onclick = () => {
         darkmode.classList.replace('bx-sun', 'bx-moon');
         document.body.classList.remove('color');
     }
-}
+};
+
+
+let menu = document.querySelector('#menu-icon');
+let nav = document.querySelector('.nav');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('open');
+};
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    nav.classList.remove('open');
+};
+
+const sr = ScrollReveal({
+    distance: '70px',
+    reset: true,
+    duration: 2700, 
+})
+sr.reveal('text-box',{delay: 200, origin: 'bottom'})
+sr.reveal('.mainmenu-img',{delay: 350, origin: 'top'})
